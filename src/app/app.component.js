fdbbfec274f9fe2cf6e7cbab0850b1033fd280f3
Alex Parloti <parloti@gmail.com>
2017-06-04 15:32:57 +0200

    Centralizing content

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -15,7 +15,8 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        templateUrl: './app.component.html',
+        styleUrls: ['./app.component.css']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
